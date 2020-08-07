b090059bded19d545825071a3f1782cbda5472b8
Prasanna <prasanna@hexaware.com>
2020-08-07 13:01:25 +0530

    code

---
+++ session29/JavaScriptDemos/src/main/webapp/P17.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+function show() {
+	var myArray = [	"Today We are learning JavaScript",
+		"Next Topic SpringHibernateNate Integration",
+		"Weekend Project will be given for Practice"
+		];
+	document.getElementById("p1").innerHTML=myArray[0];
+	document.getElementById("p2").innerHTML=myArray[1];
+	document.getElementById("p3").innerHTML=myArray[2];
+}
